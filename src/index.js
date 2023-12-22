539208af7524a7c0b59df508937a3c331ecbccac
Hoang Bach <bach.nh@smartmind.vn>
2023-12-22 14:19:17 +0700

    update code

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require('dotenv');
 const { default: mongoose } = require("mongoose");
 const routes = require("./routes");
+const cors = require('cors')
 const bodyParser = require("body-parser");
 dotenv.config()
 
@@ -11,7 +12,7 @@ const port = process.env.port || 3001
 app.get('/',(req,res) => {
     return res.send('Hello World 123456789')
 })
-
+app.use(cors())
 app.use(bodyParser.json())
 
 routes(app);
